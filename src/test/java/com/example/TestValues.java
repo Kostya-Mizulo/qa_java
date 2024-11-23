cde8f4ceebc633e57120cecdda4485ecff29bebc
Konstantin Mizulo <kosyak.mizulo@gmail.com>
2024-11-23 18:24:35 +0300

    create tests for cat

--- src/test/java/com/example/TestValues.java
+++ src/test/java/com/example/TestValues.java
@@ -11,4 +11,5 @@ public class TestValues {
     public static List<String> PREDATORS_FOOD_LIST = List.of("Животные", "Птицы", "Рыба");
     public static String UNSUPPORTED_ANIMAL_TYPE_EXCEPTION = "Неизвестный вид животного, используйте значение Травоядное или Хищник";
     public static String GET_FAMILY_ANIMAL_TEXT = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+    public static String MYAU = "Мяу";
 }
